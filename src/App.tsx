dfb01b8f03dd8fe06be31f98602190179e8fd0f8
Misho <chapidze.misho95@gmail.com>
2024-03-03 15:58:55 +0400

    s

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div className="grid grid-cols-4 w-[1110px] h-[572px]">
+        <div className="col-span-2">1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
